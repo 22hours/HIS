062444a7268d3da0f68e3d23a4b6339b3ddbbe0a
이정환 <46296754+winterlood@users.noreply.github.com>
2019-07-22 21:22:03 +0900

    NOTEPAD Hook

---
+++ 15JeongHwan/APIHookinig/NotepadHookV0/CPP/hkmain/hkmain/소스.cpp
@@ -0,0 +1,26 @@
+#include "stdio.h"
+#include "conio.h"
+#include "windows.h"
+
+#define DEF_DLL_NAME  "KeyHook.dll"
+#define DEF_HOOKSTART  "HookStart"
+#define DEF_HOOKSTOP  "HookStop"
+
+typedef void (*PFN_HOOKSTART)();
+typedef void (*PFN_HOOKSTOP)();
+
+void main()
+{
+	HMODULE   hDll = NULL;
+	PFN_HOOKSTART HookStart = NULL;
+	PFN_HOOKSTOP HookStop = NULL;
+	char   ch = 0;
+	hDll = LoadLibrary(DEF_DLL_NAME);
+	HookStart = (PFN_HOOKSTART)GetProcAddress(hDll, DEF_HOOKSTART);
+	HookStop = (PFN_HOOKSTOP)GetProcAddress(hDll, DEF_HOOKSTOP);
+	HookStart();
+	printf("press 'q' to quit!\n");
+	while (_getch() != 'q');
+	HookStop();
+	FreeLibrary(hDll);
+}
