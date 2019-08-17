ca8c3da14d7c40aa10fb680505d6c479fc5aa9ae
jongwuner <jklh0202@naver.com>
2019-08-18 06:52:31 +0900

    InjectionCode & DllMain code 추가
    - InjectionCode : 정상작동
      - splwow64.exe 올바르게 호출됨.
    
    - DllMain : 정상작동.
      - TypressOuath 정상 호출.
      - Return 값 올바르게 반환.
    - To-do : dllmain을 대체하여, registry에 모든 생성되는 프로세스에 typress.dll injection 시킨 후, process이름이 'splwow64.exe'일 경우, 동작시킨다!
    
     * 효율적인 방법은 아닌 것 같으나, 달리 방법을 찾지 못함.

---
+++ 14JongChu/[typress]DllCode/jjongchuDll1/dllmain.cpp
@@ -0,0 +1,43 @@
+﻿// dllmain.cpp : DLL 응용 프로그램의 진입점을 정의합니다.
+#include "stdafx.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+	case DLL_PROCESS_ATTACH: {
+
+		STARTUPINFO		si1 = { 0, };
+		PROCESS_INFORMATION		pi1;
+		DWORD	return_val1;
+		TCHAR	command1[] = TEXT("C:\\Users\\jklh0\\source\\github\\HIS\\Client\\HISClient\\HISClient\\bin\\Debug\\HISClient.exe"); // 경로 수정필요.
+
+		si1.cb = sizeof(si1);
+		CreateProcess(NULL, command1, NULL, NULL, TRUE, 0, NULL, NULL, &si1, &pi1);	// CreateProcess 1
+		CloseHandle(pi1.hThread);
+
+
+
+		WaitForSingleObject(pi1.hProcess, INFINITE); // 자식 프로세스의 종료를 기다리지 않으면 자식 프로세스가
+													 // 종료되기도 전에 아래 코드들이 실행되어 다른 결과를 초례할 수 있다.
+
+		GetExitCodeProcess(pi1.hProcess, &return_val1);		// 자식 프로세스에서 반환한 종료코드(Exit Code)를 return_val1에다가.
+															// Return 0, -1 : 프로그램 종료.
+															// Return 1 : 인쇄물 '출력'
+
+		if (return_val1 == -1 || return_val1 == 0) {
+			return 0;
+		}
+		else {
+			//PrintOut()
+		}
+		CloseHandle(pi1.hProcess);
+
+		break;
+    }
+    return TRUE;
+}
+
