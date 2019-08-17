3f7dea80dc0296f6c3bb7afe55ba0449db25f14e
jongwuner <jklh0202@naver.com>
2019-08-18 00:26:53 +0900

    ProcessReturnValue 추가
    - Program_Main.cs : 실행하는 모체. Target.cs의 main return값을 그대로 가져온다.
    
    - Program_Target.cs : return 값을 반환하는 대상.

---
+++ 14JongChu/ProcessReturnValue/Program_Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var proc = new Process();
+            proc.StartInfo.FileName = "C:\\Users\\jklh0\\source\\repos\\ConsoleApp5\\ConsoleApp5\\bin\\Debug\\ConsoleApp5.exe";
+            proc.Start();
+            proc.WaitForExit();
+
+            if(proc.ExitCode == 10)
+            {
+                Console.WriteLine("code 10");
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
---
+++ 14JongChu/ProcessReturnValue/Program_Target.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            return 10;
+        }
+    }
+}
